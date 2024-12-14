6c7874ca5eebf95e31e389ef05017e97904b3db3
Ahmed-AboSabra <115577304+Ahmed-AboSabra@users.noreply.github.com>
2024-12-14 15:44:32 +0200

    Update add_student_to_class.php

--- classes/add_student_to_class.php
+++ classes/add_student_to_class.php
@@ -10,18 +10,18 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 
-$student_ids_raw = $_POST['student_ids'] ?? null; 
+$student_ids = $_POST['student_ids'] ?? null; 
 $class_id = $_POST['class_id'] ?? null;
 $language = $_POST['language'] ?? null;
 
-if (empty($student_ids_raw) || empty($class_id) || empty($language)) {
+if (empty($student_ids) || empty($class_id) || empty($language)) {
     $errorMsg = ($language == "ar") ? "خطأ: الرجاء ملء جميع الحقول المطلوبة."
      : "Error: Please fill in all required fields.";
     echo json_encode(array("status" => "fail", "message" => $errorMsg));
     exit;
 }
 
-$student_ids = json_decode($student_ids_raw, true);
+$student_ids = json_decode($student_ids, true);
 
 if (!is_array($student_ids)) {
     $errorMsg = ($language == "ar") ? "  خطأ: معرفات الطلاب يجب أن تكون في صيغة قائمة مثل [1,2]" 
